335c23676cc06b797782209e001936a85cea770a
mstylski <m.stylski@o2.pl>
2023-03-23 10:05:28 +0100

    naming fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,10 @@ import { Component } from '@angular/core';
 export class AppComponent {
 
   export() {
-    let data = JSON.stringify({ wether: { is_sunny: true, temp: '+25' } });
+    let data = JSON.stringify({ weather: { is_sunny: true, temp: '+25' } });
     let dataUri =
       'data:application/json;charset=utf-8,' + encodeURIComponent(data);
-    let exportFileName = 'wether.json';
+    let exportFileName = 'weather.json';
     let linkElement = document.createElement('a');
     linkElement.setAttribute('href', dataUri);
     linkElement.setAttribute('download', exportFileName);
