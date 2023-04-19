3a3414faac1c6a008131d6ad95dbeca6fa3f6d91
elena.gronskaya <elena.gronskaya@gmail.com>
2023-04-19 19:08:51 +0200

    car-search/task3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,14 @@ import {MatListModule} from '@angular/material/list';
 import { WidgetComponent } from './widget/widget.component';
 import {WeatherWidgetContentComponent} from "./widget/weather-widget/weather-widget.component";
 import {SprintWidgetContentComponent} from "./widget/sprint-widget/sprint-widget.component";
+import { CarSearchComponent } from './car-search/car-search.component';
+import {HttpClientModule} from "@angular/common/http";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent,
-    WidgetComponent, WeatherWidgetContentComponent, SprintWidgetContentComponent
+    WidgetComponent, WeatherWidgetContentComponent, SprintWidgetContentComponent, CarSearchComponent
   ],
   imports: [
     BrowserModule,
@@ -21,6 +24,9 @@ import {SprintWidgetContentComponent} from "./widget/sprint-widget/sprint-widget
     MatListModule,
     MatButtonModule,
     MatIconModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   bootstrap: [AppComponent]
 })
---
+++ src/app/interfaces/car-filter.ts
@@ -0,0 +1,23 @@
+export interface ICarFilter {
+  model: string,
+  limit: number,
+}
+
+export interface ICar{
+  make: string,
+  model: string,
+  transmission: string,
+  class:string,
+  year: number
+}
+
+
+export interface  ICarResponse{
+    make: string,
+    model: string,
+    transmission: string,
+    class:string,
+    year: number
+}
+
+
